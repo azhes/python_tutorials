2f25db054648110e303d31c84a34b411804f211e
Azhes <agraystone@AnthemProperties.com>
2023-05-15 15:21:39 -0700

    finishes first exercise

--- pokemon_list.py
+++ pokemon_list.py
@@ -7,6 +7,15 @@
 # Step 1: Store favourite Pokemon in a list
 favourite_pokemon = []
 
+# Step 2: Loop through each Pokemon in the list
+for :
+    # Step 3: Check if the length of the Pokemon's name is even or odd
+    if :
+        # Step 4: Print a message for each Pokemon indicating its name and whether the name length is even or odd
+        print()
+    else:
+        print()
+
 
 
 
