3e5647708fa350fbf6f77c9177aa115a02c8f247
Andy <77294025+azhes@users.noreply.github.com>
2023-05-15 15:16:14 -0700

    Add files via upload

---
+++ pokemon_list.py
@@ -0,0 +1,13 @@
+# Task: Write a Python program that:
+# 1. Creates a list of 5 Pokemon
+# 2. Loops through each Pokemon in the list
+# 3. Checks if the length of the Pokemon's name is even or odd
+# 4. Prints a message for each Pokemon indicating its name and whether the name length is even or odd
+
+# Step 1: Store favourite Pokemon in a list
+favourite_pokemon = []
+
+
+
+
+
